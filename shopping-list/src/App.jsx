2d34a0fc2fafd31b2de7317adc974503a2bad2a3
Daniel Vono <sand.doomers@gmail.com>
2024-11-18 15:56:30 -0500

    Added basic functionality and some styling

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -1,9 +1,74 @@
 // Step 4. Import React and useState
-
+import React, {useState} from 'react';
 // Step 5. Import the App.css file
-
+import './App.css'
 // Step 6. Create a functional component called App
+export default function App()
+{
+    //Runs Javascript
 
+    //Creates a React Hook for ___
+    const [tasks, setTasks] = useState([]);
+
+    //Creates a React Hook for ___
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event) => 
+    {
+        setInputValue(event.target.value);
+    };
+
+    const handleAddTask = () =>
+    {
+        if(inputValue.trim())
+        {
+            //Adds the inputValue to the tasks array
+            setTasks([...tasks, inputValue]);
+
+            setInputValue('');
+        }
+    };
+
+    const handleDeleteTask = (index) =>
+    {
+        //Copies the current array of items except for the item to delete
+        const newTaskList = tasks.filter((task, i) => i != index)
+
+        setTasks(newTaskList);
+    };
+
+    //Returns JSX, {} inserts Javascript
+    return (
+        <div className='list'>
+            <h1>🛒 Shopping List 🛒</h1>
+
+            <div class='logo'>
+                <img src='/logo192.png' alt='Logo' class='logo'></img>
+            </div>
+
+            <div className='input'>
+                <input
+                    type = 'text'
+                    value = {inputValue}
+                    onChange={handleInputChange}
+                    placeholder='Enter a new item'
+                ></input>
+
+                <button onClick={handleAddTask}>Add</button>
+            </div>
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index} className='task-item'>
+                        {task}
+                        <div class='task-actions'>
+                            <button onClick={() => handleDeleteTask(index)}>🗑️</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
     // Step 8a. Initialize useState state variables
 
     
@@ -35,7 +100,3 @@
             
             
                 {/* Step 10a. Iterate over every task and retrieve its index*/}
-                
-
-
-
